76c44a29964a84e541e5cac7e884d6a552b0384f
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2025-10-15 13:47:58 -0400

    feat: adding electron

---
+++ electron/main.cjs
@@ -0,0 +1,34 @@
+// electron/main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // optional
+    },
+  });
+
+  // In dev mode, load from Vite's dev server
+  if (process.env.NODE_ENV === 'development') {
+    console.log('Loading from Vite dev server');
+    win.loadURL('http://localhost:5173');
+  } else {
+    // In production, load the built files
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
