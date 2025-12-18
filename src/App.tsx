d96c698f0c5f91b4e2f3ab1aac8bde2edea59a9e
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2025-12-18 15:53:13 -0400

    feat: adding save request button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Box, Button, TextField } from "@mui/material"
-import { useQuery } from "@tanstack/react-query";
-import { fetchApi } from "./hooks/useRequest/httpRequest";
 import { MethodSelect } from "./components";
 import { HttpMethod } from "./types/http";
 import { useRequest } from "./hooks";
@@ -9,6 +7,10 @@ import { useRequest } from "./hooks";
 const App = () => {
   const [url, setUrl] = React.useState("");
   const [method, setMethod] = React.useState<HttpMethod>('GET');
+
+  React.useEffect(() => {
+    console.log("Method changed to:", method);
+  }, [method]);
 
   const {
     data,
@@ -25,16 +27,17 @@ const App = () => {
     execute();
   }
 
-  React.useEffect(() => {
-    console.log("Method changed to:", method);
-  }, [method]);
+  const handleSaveRequest = () => {
+    // Implement save request logic here
+    console.log("Request saved:", { method, url });
+  }
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: 800, margin: 'auto', marginTop: 5 }}>
       <Box sx={{ display: 'flex', gap: 2, flex: 1}}>
         <MethodSelect sx={{flex: 2, maxWidth: 120}} value={method} onChange={setMethod} />
         <TextField
-          sx={{ flex: 8 }}
+          sx={{ flex: 6 }}
           id="outlined-basic"
           label="URL"
           variant="outlined"
@@ -42,6 +45,7 @@ const App = () => {
           onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
             setUrl(event.target.value);
           }} />
+        <Button sx={{ flex: 2, maxWidth: 120 }} variant="outlined" onClick={handleSaveRequest}>Save</Button>
         <Button sx={{ flex: 2, maxWidth: 120 }} variant="contained" onClick={handleSendRequest}>SEND</Button>
       </Box>
       <Box sx={{ flex: 1, display: 'flex' }}>
