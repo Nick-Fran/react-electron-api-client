c21bb8dd07ac1dbdcb6baa1fb2039920bd77923f
Nicolas Queiroz <nicolas.queirozvf@gmail.com>
2025-12-18 12:39:04 -0400

    feat: adding url field and send button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { Box, Button, TextField } from "@mui/material"
+import React from "react";
+
 const App = () => {
 
+  const [url, setUrl] = React.useState("");
+
+  const handleSendRequest = () => {
+    console.log("Sending request to ", url)
+  }
+
   return (
-    <>
-      Hello World
-    </>
+    <Box sx={{ display: 'flex', gap: 2, width: 400, margin: 'auto', marginTop: 5 }}>
+      <TextField
+        sx={{ flex: 10 }}
+        id="outlined-basic"
+        label="URL"
+        variant="outlined"
+        value={url}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+          setUrl(event.target.value);
+        }} />
+      <Button sx={{ flex: 2 }} variant="contained" onClick={handleSendRequest}>SEND</Button>
+    </Box>
   )
 }
 
